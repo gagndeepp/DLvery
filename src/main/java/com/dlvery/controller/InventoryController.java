ea8e9ae6736a7f7fe9eae0dba5071bcb62791201
legen <legen@Steve>
2021-04-19 17:08:14 +0530

    CrossOrigin added to Controller class & working with React front end now.

--- src/main/java/com/dlvery/controller/InventoryController.java
+++ src/main/java/com/dlvery/controller/InventoryController.java
@@ -3,17 +3,21 @@ package com.dlvery.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import com.dlvery.model.Inventory;
 import com.dlvery.service.InventoryService;
 
 @RestController
 @RequestMapping(value = "inventory")
+@CrossOrigin(origins="http://localhost:3000")
+//@CrossOrigin(origins="http://192.168.1.62")
 public class InventoryController {
 	@Autowired
 	private InventoryService is;
@@ -22,6 +26,7 @@ public class InventoryController {
 	 * public InventoryController(inventoryService i) { // TODO Auto-generated
 	 * constructor stub this.is = i; }
 	 */
+	
 
 	@GetMapping("/all")
 	public List<Inventory> allInventory() {
